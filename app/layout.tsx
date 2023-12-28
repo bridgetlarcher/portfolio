f2531af8bd8307d60ad176b8c3e0b632db3442f3
bridgetlarcher <bridgetlarcher@gmail.com>
2023-12-28 00:57:53 -0500

    Add speed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -43,6 +44,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`${roboto.className} leading-relaxed text-slate-400`}>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   )
 }
